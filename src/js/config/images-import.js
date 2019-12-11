af3144d23f2b1617c7d54a64dd3b6e0e99ca793b
Egor <meekgot@gmail.com>
2019-12-11 14:22:37 +0300

    Feat: created services screen

--- src/js/config/images-import.js
+++ src/js/config/images-import.js
@@ -5,3 +5,9 @@ import '../../images/dot-grid.png';
 import '../../images/main-screen-slide01.jpg';
 import '../../images/main-screen-slide02.jpg';
 import '../../images/main-screen-slide03.jpg';
+
+// services images
+import '../../images/services-image01.jpg';
+import '../../images/services-image02.jpg';
+import '../../images/services-image03.jpg';
+import '../../images/services-image04.jpg';
